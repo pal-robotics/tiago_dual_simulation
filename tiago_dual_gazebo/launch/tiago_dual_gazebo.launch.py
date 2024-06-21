908bea50db2b968304015b9b493c3b0acd74472b
David ter Kuile <david.terkuile@pal-robotics.com>
2024-06-21 09:57:20 +0200

    Change import for launch args

--- tiago_dual_gazebo/launch/tiago_dual_gazebo.launch.py
+++ tiago_dual_gazebo/launch/tiago_dual_gazebo.launch.py
@@ -25,8 +25,9 @@ from launch.substitutions import LaunchConfiguration
 from launch_pal.include_utils import include_scoped_launch_py_description
 
 from launch_ros.actions import Node
-from launch_pal.arg_utils import LaunchArgumentsBase, CommonArgs
-from launch_pal.robot_arguments import TiagoDualArgs
+from launch_pal.arg_utils import LaunchArgumentsBase
+from launch_pal.robot_arguments import CommonArgs
+from tiago_dual_description.launch_arguments import TiagoDualArgs
 from dataclasses import dataclass
 from launch_pal.actions import CheckPublicSim
 
